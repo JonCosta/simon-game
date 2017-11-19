f79f4c53946c202b28065089a3ed9f477adebe03
Jonathan Costa <jonjcosta@gmail.com>
2017-11-19 14:09:13 -0200

    Basic logic and style
    - Added functions for starting game, also for counting and checking step
    - Began adding style for the game circle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,86 @@
-$(function() {
+$(function () {
 
-    var colors = ["green", "blue", "yellow", "red"];
-    var gameSequence = [];
-    var yourSequence = [];
+    var Simon = {
+        colors: ["green", "blue", "yellow", "red"],
+        colorSounds: {
+            green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+            blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+            yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+            red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+        },
+        targetSequence: [],
+        playerSequence: [],
 
-    $(".btn__start").click(function (e) { 
-        e.preventDefault();
-        gameSequence = [];
-        simonGame();
-    });
+        startGame: function () {
+            console.log("Game started");
+            this.targetSequence = [];
+            this.playerSequence = [];
+            this.addToSequence();
+            this.presentSequence();
+        },
 
-    $(".block__green").click(function (e) { 
-        e.preventDefault();
-        yourSequence.push("green");
-        if (yourSequence.length == gameSequence.length) checkSequences();        
-    });
+        addToSequence: function () {
+            let randomIndex = Math.floor(Math.random() * 4);
+            this.targetSequence.push(this.colors[randomIndex]);
+            console.log(this.targetSequence);
+        },
 
-    $(".block__red").click(function (e) { 
-        e.preventDefault();
-        yourSequence.push("red");
-        if (yourSequence.length == gameSequence.length) checkSequences();        
-    });
+        presentSequence: function () {
+            console.log("Presenting steps");
+            for (let i = 0; i < this.targetSequence.length; i++) {
+                const step = this.targetSequence[i];
+                console.log("Step", step);
+                this.colorSounds[step].play();
+                sleep(600);
+                this.colorSounds[step].pause();
+            }
+        },
 
-    $(".block__yellow").click(function (e) { 
-        e.preventDefault();
-        yourSequence.push("yellow");
-        if (yourSequence.length == gameSequence.length) checkSequences();        
-    });
+        checkSequences: function () {
+            console.log("Checking sequences");
+            for (let i = 0; i < this.playerSequence.length; i++) {
+                if (this.playerSequence[i] != this.targetSequence[i]) {
+                    console.log("Wrong!");
+                    return false;
+                    this.presentSequence();
+                    this.playerSequence = [];
+                }
+            }
 
-    $(".block__blue").click(function (e) { 
-        e.preventDefault();
-        yourSequence.push("blue");
-        if (yourSequence.length == gameSequence.length) checkSequences();
-    });
-
-    function checkSequences() {
-        for (let index = 0; index < yourSequence.length; index++) {
-            if (yourSequence[index] != gameSequence[index]) {
-                console.log("Wrong!");
+            console.log("Correct!");
+            if (this.playerSequence.length == 20) {
+                // WINNER
                 return false;
             }
-        }
-        console.log("Correct!");
-        simonGame();
-        return true;
-    } // endof checkSequences()
-    
-    function simonGame() {
-        if (!gameSequence.length) {
-            restart();
-        } else {
-            let randomIndex = Math.floor(Math.random() * 4);
-            gameSequence.push(colors[randomIndex]);
-        }
-        // Função para tocar música de cada bloco
-        console.log(gameSequence);        
-        yourSequence = [];
-    } // endof simonGame()
 
-    function restart() {
-        for (let index = 0; index < 3; index++) {
-            let randomIndex = Math.floor(Math.random() * 4);
-            gameSequence.push(colors[randomIndex]);
+            if (this.playerSequence.length == this.targetSequence.length) {
+                this.addToSequence();
+                this.presentSequence();
+                this.playerSequence = [];
+            }
         }
-    } // endof restart()
+
+    };
+
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (let i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
+
+    $(".btn__start").click(function (e) {
+        e.preventDefault();
+        Simon.startGame();
+    });
+
+    $('.circle-quarter').click(function (e) {
+        e.preventDefault();
+        let color = $(this).data('color');
+        Simon.playerSequence.push(color);
+        Simon.checkSequences();
+    });
 
 });
