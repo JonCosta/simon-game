e707c4b704aa2e5b57a291b579f0efe195a10337
Jonathan Costa <jonjcosta@gmail.com>
2017-11-21 22:05:24 -0200

    Forming a circle and more sounds
    - Clicking on buttons now triggers their sound
    - Adjusted circle quarters to form a full circle together

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,67 @@
 $(function () {
 
-    var Simon = {
-        colors: ["green", "blue", "yellow", "red"],
-        colorSounds: {
-            green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-            blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-            yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-            red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
-        },
-        targetSequence: [],
-        playerSequence: [],
+    var colors = ["green", "blue", "yellow", "red"];
+    var colorSounds = {
+        green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+        blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+        yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+        red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+    };
+    var targetSequence = [];
+    var playerSequence = [];
 
-        startGame: function () {
-            console.log("Game started");
-            this.targetSequence = [];
-            this.playerSequence = [];
-            this.addToSequence();
-            this.presentSequence();
-        },
+    function startGame() {
+        console.log("Game started");
+        targetSequence = [];
+        playerSequence = [];
+        addToSequence();
+        presentSequence();
+    }
 
-        addToSequence: function () {
-            let randomIndex = Math.floor(Math.random() * 4);
-            this.targetSequence.push(this.colors[randomIndex]);
-            console.log(this.targetSequence);
-        },
+    function addToSequence() {
+        let randomIndex = Math.floor(Math.random() * 4);
+        targetSequence.push(colors[randomIndex]);
+        console.log(targetSequence);
+    }
 
-        presentSequence: function () {
-            console.log("Presenting steps");
-            for (let i = 0; i < this.targetSequence.length; i++) {
-                const step = this.targetSequence[i];
-                console.log("Step", step);
-                this.colorSounds[step].play();
-                sleep(600);
-                this.colorSounds[step].pause();
-            }
-        },
+    function presentSequence() {
+        console.log("Presenting steps");
+        for (let i = 0; i < targetSequence.length; i++) {
+            const step = targetSequence[i];
+            console.log("Step", step);
+            colorSounds[step].play();
+            setTimeout(() => {
+                colorSounds[step].pause();
+                colorSounds[step].currentTime = 0;
+            }, 600);
+            sleep(700);
+        }
+    }
 
-        checkSequences: function () {
-            console.log("Checking sequences");
-            for (let i = 0; i < this.playerSequence.length; i++) {
-                if (this.playerSequence[i] != this.targetSequence[i]) {
-                    console.log("Wrong!");
-                    return false;
-                    this.presentSequence();
-                    this.playerSequence = [];
-                }
-            }
-
-            console.log("Correct!");
-            if (this.playerSequence.length == 20) {
-                // WINNER
+    function checkSequences() {
+        console.log("Checking sequences");
+        for (let i = 0; i < playerSequence.length; i++) {
+            if (playerSequence[i] != targetSequence[i]) {
+                console.log("Wrong!");
+                sleep(1000);
                 return false;
-            }
-
-            if (this.playerSequence.length == this.targetSequence.length) {
-                this.addToSequence();
-                this.presentSequence();
-                this.playerSequence = [];
+                presentSequence();
+                playerSequence = [];
             }
         }
 
-    };
+        console.log("Correct!");
+        if (playerSequence.length == 20) {
+            // WINNER
+            return false;
+        }
+
+        if (playerSequence.length == targetSequence.length) {
+            addToSequence();
+            presentSequence();
+            playerSequence = [];
+        }
+    }
 
     function sleep(milliseconds) {
         var start = new Date().getTime();
@@ -71,16 +72,21 @@ $(function () {
         }
     }
 
-    $(".btn__start").click(function (e) {
+    $(".btn-start").click(function (e) {
         e.preventDefault();
-        Simon.startGame();
+        startGame();
     });
 
     $('.circle-quarter').click(function (e) {
         e.preventDefault();
         let color = $(this).data('color');
-        Simon.playerSequence.push(color);
-        Simon.checkSequences();
+        playerSequence.push(color);
+        colorSounds[color].play();
+        setTimeout(() => {
+            colorSounds[color].pause();
+            colorSounds[color].currentTime = 0;
+        }, 600);
+        checkSequences();
     });
 
 });
